d43fabc2e1982df6723c52e7c5e2f4e1284c1b80
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-22 07:34:32 +0900

    Fix tar command to extract completion file

--- update_completion.sh
+++ update_completion.sh
@@ -2,7 +2,8 @@
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.tar.gz
 
 # extract the zsh completion file to the current directory
-tar xf yt-dlp.tar.gz yt-dlp/completions/zsh/_yt-dlp --strip=3
+tar --strip=3 -xf yt-dlp.tar.gz yt-dlp/completions/zsh/_yt-dlp
 
 # remove the downloaded file
 rm -f yt-dlp.tar.gz
+
