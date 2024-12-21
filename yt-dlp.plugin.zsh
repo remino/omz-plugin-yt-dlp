a0a21adc3008102fad5856eaee6e4f817c85bb59
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-22 08:06:05 +0900

    Refactor yt-dlp function:
    - Move function into autoload file.
    - Add axel and mp4 arg aliases.
    - Alias yd as yt-dlp.

--- yt-dlp.plugin.zsh
+++ yt-dlp.plugin.zsh
@@ -1,20 +1,12 @@
-unalias yd 2> /dev/null || true
-
-function yd() {
-  local output_format="%(upload_date)s - %(uploader)s - %(title)s [%(width)sx%(height)s %(fps)s][%(display_id)s].%(ext)s"
-  local alias_noarc="--no-download-archive"
-  local alias_sub="--sub-langs all --convert-subs srt --mkv --embed-subs"
-  local alias_mkv="--remux-video mkv"
-  local param_def="-N 10 --download-archive done.txt --embed-chapters"
-
-  yt-dlp -o $output_format \
-		--alias narc $alias_noarc \
-		--alias sub $alias_sub \
-		--alias mkv $alias_mkv \
-		--download-archive "done.txt" \
-		--embed-chapters \
-		-N 10 \
-		"$@"
+_ytdlp_alias_set() {
+	fpath+=($(dirname "$0"))
+	autoload -Uz yt-dlp
 }
 
+_ytdlp_alias_set
+
+unset -f _ytdlp_alias_set
+
+unalias yd 2> /dev/null || true
+alias yd=yt-dlp
 compdef yd=yt-dlp
