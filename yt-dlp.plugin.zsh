dc03a87ccfa515962b85517960c9595763669d11
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-22 07:35:52 +0900

    Refactor y function, rename to yd

--- yt-dlp.plugin.zsh
+++ yt-dlp.plugin.zsh
@@ -1,18 +1,20 @@
-unalias y 2> /dev/null || true
+unalias yd 2> /dev/null || true
 
-function y() {
-  local outputFormat="%(upload_date)s - %(uploader)s - %(title)s [%(width)sx%(height)s %(fps)s][%(display_id)s].%(ext)s"
-  local noArcAlias="--no-download-archive"
-  local subAlias="--sub-langs all --convert-subs srt --mkv --embed-subs"
-  local mkvAlias="--remux-video mkv"
-  
-  local defParam="-N 10 --download-archive done.txt --embed-chapters"
-  
-  if [[ -f "./yt.config.txt" ]]; then
-    echo 'Renaming ./yt.config.txt to ./yt-dlp.conf'
-    mv './yt.config.txt' './yt-dlp.conf'
-  fi
+function yd() {
+  local output_format="%(upload_date)s - %(uploader)s - %(title)s [%(width)sx%(height)s %(fps)s][%(display_id)s].%(ext)s"
+  local alias_noarc="--no-download-archive"
+  local alias_sub="--sub-langs all --convert-subs srt --mkv --embed-subs"
+  local alias_mkv="--remux-video mkv"
+  local param_def="-N 10 --download-archive done.txt --embed-chapters"
 
-  yt-dlp -o $outputFormat --alias narc $noArcAlias --alias sub $subAlias --alias mkv $mkvAlias -N 10 --download-archive "done.txt" --embed-chapters  "$@"
+  yt-dlp -o $output_format \
+		--alias narc $alias_noarc \
+		--alias sub $alias_sub \
+		--alias mkv $alias_mkv \
+		--download-archive "done.txt" \
+		--embed-chapters \
+		-N 10 \
+		"$@"
 }
-compdef y=yt-dlp
+
+compdef yd=yt-dlp
